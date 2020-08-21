dd6f17681cbb1e82f6fcb0e8cef48a384a7dd33b
belvoirwriting <69843158+belvoirwriting@users.noreply.github.com>
2020-08-21 16:41:54 +0100

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -12,7 +12,7 @@ export default {
   siteName: 'Vale of Belvoir Writing Group',
   tagline: 'Helping you find your own voice as a writer',
   featureImage: '/uploads/meeting.jpeg',
-  logo: '/uploads/vbw3.png', // 'logo-component', // or '/logo.svg' for regular image
+  logo: '/uploads/vbw6.svg', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
     id: process.env.GOOGLE_ANALYTICS_ID
