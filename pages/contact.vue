556af2e966a9ff02dbcbbcd315cb9cab56d2823a
belvoirwriting <69843158+belvoirwriting@users.noreply.github.com>
2020-10-14 08:36:12 +0100

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -18,9 +18,9 @@
               </p>
               <figure class="image is-1by1 ">
                 <opti-image
-                  :src="require('~/assets/uploads/meeting.jpg').src"
+                  :src="require('~/assets/uploads/meeting.jpeg').src"
                   :srcset="
-                    require('~/assets/uploads/meeting.jpg').srcSet
+                    require('~/assets/uploads/meeting.jpeg').srcSet
                   "
                 />
               </figure>
